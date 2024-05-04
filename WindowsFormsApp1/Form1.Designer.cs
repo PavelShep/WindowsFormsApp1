03d6e2c0ac149c84b9ace7687ed2d49ff4d073a1
Pavel Shapialevich <Pavel.Shapialevich@student.ajp.edu.pl>
2024-05-04 11:31:07 +0200

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxCountries = new System.Windows.Forms.ListBox();
+            this.listBoxRegion = new System.Windows.Forms.ListBox();
+            this.listBoxSubregion = new System.Windows.Forms.ListBox();
+            this.buttonFind = new System.Windows.Forms.Button();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBoxCountries
+            // 
+            this.listBoxCountries.FormattingEnabled = true;
+            this.listBoxCountries.Location = new System.Drawing.Point(3, 285);
+            this.listBoxCountries.Name = "listBoxCountries";
+            this.listBoxCountries.Size = new System.Drawing.Size(227, 108);
+            this.listBoxCountries.TabIndex = 0;
+            // 
+            // listBoxRegion
+            // 
+            this.listBoxRegion.FormattingEnabled = true;
+            this.listBoxRegion.Location = new System.Drawing.Point(12, 62);
+            this.listBoxRegion.Name = "listBoxRegion";
+            this.listBoxRegion.Size = new System.Drawing.Size(147, 69);
+            this.listBoxRegion.TabIndex = 1;
+            this.listBoxRegion.SelectedIndexChanged += new System.EventHandler(this.listBoxRegion_SelectedIndexChanged);
+            // 
+            // listBoxSubregion
+            // 
+            this.listBoxSubregion.FormattingEnabled = true;
+            this.listBoxSubregion.Location = new System.Drawing.Point(12, 158);
+            this.listBoxSubregion.Name = "listBoxSubregion";
+            this.listBoxSubregion.Size = new System.Drawing.Size(147, 69);
+            this.listBoxSubregion.TabIndex = 2;
+            // 
+            // buttonFind
+            // 
+            this.buttonFind.Location = new System.Drawing.Point(12, 246);
+            this.buttonFind.Name = "buttonFind";
+            this.buttonFind.Size = new System.Drawing.Size(170, 23);
+            this.buttonFind.TabIndex = 3;
+            this.buttonFind.Text = "Wczytaj kraje z regiona";
+            this.buttonFind.UseVisualStyleBackColor = true;
+            this.buttonFind.Click += new System.EventHandler(this.buttonFind_Click);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(12, 12);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(170, 23);
+            this.buttonStart.TabIndex = 4;
+            this.buttonStart.Text = "Czytaj API";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(227, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.buttonFind);
+            this.Controls.Add(this.listBoxSubregion);
+            this.Controls.Add(this.listBoxRegion);
+            this.Controls.Add(this.listBoxCountries);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxCountries;
+        private System.Windows.Forms.ListBox listBoxRegion;
+        private System.Windows.Forms.ListBox listBoxSubregion;
+        private System.Windows.Forms.Button buttonFind;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private const string ApiBaseUrl = "https://restcountries.com/v3.1/all";
+        private List<Country> allCountries;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void buttonStart_Click(object sender, EventArgs e)
+        {
+            buttonStart.Enabled = false;
+            await LoadCountriesAsync();
+        }
+
+        private async Task LoadCountriesAsync()
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    var response = await client.GetAsync(ApiBaseUrl);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var json = await response.Content.ReadAsStringAsync();
+                        // save local json
+                        var path = "../../all_countries.json";
+                        File.WriteAllText(path, json);
+
+                        allCountries = JsonConvert.DeserializeObject<List<Country>>(json);
+                        PopulateRegions(allCountries);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Failed to retrieve country data: " + response.ReasonPhrase, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error with connection to " + ApiBaseUrl + ": " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                DialogResult result = MessageBox.Show("Would you like to use local all_countries.json?", "Question", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (result == DialogResult.Yes)
+                {
+                    var path = "../../all_countries.json";
+                    var json = File.ReadAllText(path);
+                    allCountries = JsonConvert.DeserializeObject<List<Country>>(json);
+                    PopulateRegions(allCountries);
+                }
+                else
+                {
+                    MessageBox.Show("Try to restart app", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            finally
+            {
+                buttonStart.Enabled = true;
+            }
+        }
+
+        private void PopulateRegions(List<Country> countries)
+        {
+            foreach (var country in countries)
+            {
+                if (!listBoxRegion.Items.Contains(country.Region))
+                    listBoxRegion.Items.Add(country.Region);
+            }
+        }
+
+        private void PopulateSubregions(List<Country> countries, string region)
+        {
+            listBoxSubregion.Items.Clear();
+
+            foreach (var country in countries)
+            {
+                if (country.Region == region && country.Subregion != null && !listBoxSubregion.Items.Contains(country.Subregion))
+                    listBoxSubregion.Items.Add(country.Subregion);
+            }
+        }
+
+        private void listBoxRegion_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedRegion = listBoxRegion.SelectedItem?.ToString();
+            if (!string.IsNullOrEmpty(selectedRegion))
+            {
+                PopulateSubregions(allCountries, selectedRegion);
+            }
+        }
+
+        private void buttonFind_Click(object sender, EventArgs e)
+        {
+            var filteredCountries = FilterCountries(listBoxRegion.SelectedItem?.ToString(), listBoxSubregion.SelectedItem?.ToString());
+            listBoxCountries.Items.Clear();
+
+            foreach (var country in filteredCountries)
+            {
+                listBoxCountries.Items.Add(country.Name.Common);
+            }
+        }
+
+        private List<Country> FilterCountries(string region, string subregion)
+        {
+            if (allCountries == null)
+                return new List<Country>();
+
+            var filteredCountries = allCountries;
+
+            if (!string.IsNullOrEmpty(region))
+                filteredCountries = filteredCountries.FindAll(c => c.Region == region);
+
+            if (!string.IsNullOrEmpty(subregion))
+                filteredCountries = filteredCountries.FindAll(c => c.Subregion == subregion);
+
+            return filteredCountries;
+        }
+    }
+
+    public class Country
+    {
+        [JsonProperty("name")]
+        public CountryName Name { get; set; }
+
+        [JsonProperty("region")]
+        public string Region { get; set; }
+
+        [JsonProperty("subregion")]
+        public string Subregion { get; set; }
+    }
+
+    public class CountryName
+    {
+        [JsonProperty("common")]
+        public string Common { get; set; }
+    }
+}
